5603f30134460106277631afb8be64e3801c7d3e
sharma66mahesh <gauravpudel1990@gmail.com>
2020-11-05 11:17:08 +0545

    Update the UI texts for payouts

--- dice_frontend/src/App.js
+++ dice_frontend/src/App.js
@@ -356,7 +356,7 @@ class App extends Component {
             <p></p>
             <p> Bet Results: {result} </p>
             <p></p>
-            <p> Main Bet Amount: {this.state.mainPayoutAmount} ICX<br/> Side Bet Amount: {this.state.sidePayoutAmount} ICX</p>
+            <p> Main Bet Payout: {this.state.mainPayoutAmount} ICX<br/> Side Bet Payout: {this.state.sidePayoutAmount} ICX</p>
             </Col>
             <Col md={3} className='right-col'>
               <p>Deployer Address: {deployer_wallet.getAddress()}</p>
